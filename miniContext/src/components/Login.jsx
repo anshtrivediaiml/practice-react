fbf39c114931ee7a8235ee5eabdec82ee0e9d0ed
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-06-13 12:50:25 +0530

    themeswitcher for learning contextAPI

--- miniContext/src/components/Login.jsx
+++ miniContext/src/components/Login.jsx
@@ -14,13 +14,13 @@ function Login() {
     }
   return (
     <div className='max-w-md mx-auto'>
-        <div className='px-4 rounded-lg mt-32 drop-shadow-md border-2 backdrop-blur-sm'>
-            <h1 className='text-2xl text-muted-foreground font-semibold pt-2'>Login</h1>
+        <div className='px-4 rounded-lg mt-32 drop-shadow-md border-2 backdrop-blur-sm min-h-96'>
+            <h1 className='text-4xl text-muted-foreground font-semibold pt-8 pb-6'>Login</h1>
             <div className=' mt-5 flex flex-col gap-2'>
-                <input type='text' placeholder='username' value={username} onChange={(e)=>setUserName(e.target.value)} className='border-2 p-2 pb-2 bg-gray-400/5 rounded-lg outline-none '/>
+                <input type='text' placeholder='username' value={username} onChange={(e)=>setUserName(e.target.value)} className='border-2 p-4 pb-4 bg-gray-400/5 rounded-lg outline-none '/>
 
-               <input type='password' placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)} className='border-2  p-2 rounded-md bg-gray-400/5 outline-none mb-3 pb-3'/>
-               <button className='bg-blue-500 p-2 rounded-lg text-white font-semibold mb-5 hover:bg-blue-700 duration-200' onClick={handleSubmit}>
+               <input type='password' placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)} className='border-2  p-4 rounded-md bg-gray-400/5 outline-none mb-3 pb-4'/>
+               <button className='bg-blue-500 p-4 rounded-lg text-white font-semibold mb-5 hover:bg-blue-700 duration-200' onClick={handleSubmit}>
                 Login
                </button>
             </div>
