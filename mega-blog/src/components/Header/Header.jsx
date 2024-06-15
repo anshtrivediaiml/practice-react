4fb912d0ac962a9449c45cca1b04772a0ebbfebb
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-06-15 20:08:07 +0530

    Header,Footer and Input  compoent initialized

--- mega-blog/src/components/Header/Header.jsx
+++ mega-blog/src/components/Header/Header.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import Container from '../container/Container'
+import Logo from '../Logo'
+import LogoutBtn from './LogoutBtn'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { logout } from '../../store/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+const authStatus=useSelector((state)=>{
+ state.auth.status
+})  
+const navigate = useNavigate()
+
+const NavItems=[
+ {name:'Home', slug:'/',active:true},
+  {name:"Login",slug:"/login",active:!authStatus},
+{name:'SignUp',slug:'/signup',active:!authStatus},
+{name:'All Posts',slug:'/all-posts',active:authStatus},
+{name:'Add Post',slug:'/add-post',active:authStatus},]
   return (
-    <div>
-      header
-    </div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+           <Link to="/">
+           <Logo width='70px'/>
+           </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {NavItems.map((item)=>
+            item.active ? (
+              <li key={item.name}>
+                <button onClick={()=> navigate(item.slug)}className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                  {item.name}
+                </button>
+              </li>
+            ):null)}
+            {authStatus &&(
+              <li className=''>
+                <LogoutBtn/>
+                </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+      
+    </header>
   )
 }
 
